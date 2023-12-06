9d1193f3c031a142316d160fc66176e32e57e7f5
Martin Apresa <japresaperez@gmail.com>
2023-12-06 10:03:12 -0700

    added box shadow, GitHub & LinkedIn

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
+function gitHubOnClick() {
+  window.open('https://github.com/apin1992');
+}
+function linkedInOnClick() {
+  window.open('https://www.linkedin.com/in/martin-perez-313985273/');
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const box = document.querySelector('.profile-img');
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function updateBoxShadow() {
+    const color = getRandomColor();
+    const boxShadowValue = `0 0 30px ${color}`;
+    box.style.boxShadow = boxShadowValue;
+  }
+  setInterval(updateBoxShadow, 1500);
+});
